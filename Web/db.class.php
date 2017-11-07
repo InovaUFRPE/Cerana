f9aac25f991b33524f1efec87dd302508611646d
Unknown <luiz.c.brito@Hotmail.com>
2017-11-06 22:23:15 -0300

    Site New Version

---
+++ Web/db.class.php
@@ -0,0 +1,27 @@
+<<?php 
+
+	class db{
+	
+		private $host = 'localhost';
+		private $usuario = 'root';
+		private $senha = '';
+		private $database = 'projeto_cerana';
+
+		public function conecta_mysql(){
+
+			$con = mysqli_connect($this->host,$this->usuario,$this->senha,$this->database);
+
+			mysqli_set_charset($con, 'utf8');
+
+			if(mysqli_connect_errno()){
+            	echo 'Erro ao tentar se conectar com BD MySQL: ' .mysqli_connect_error();
+        	}
+
+        return $con;
+
+	}
+
+
+}
+
+ ?>
